66ccd304bc98bfcdb096cc8bf3398187683880d2
abbytagos <abbytagos@gmail.com>
2023-03-16 17:26:45 +1300

    miniproject3 initial commit

---
+++ frontEnd/src/components/Navbar.jsx
@@ -0,0 +1,106 @@
+import { Search, ShoppingBagOutlined } from '@mui/icons-material';
+import { Badge } from '@mui/material';
+import React from 'react'
+import styled from 'styled-components'
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import { BrowserRouter,Routes,Route,useNavigate,Link } from 'react-router-dom';
+
+
+const Container = styled.div`
+    height: 80px;
+`;  
+
+const Wrapper = styled.div`
+    padding: 10px 20px; 
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+
+
+const SearchContainer = styled.div`
+     border: 0.5px solid lightgray; 
+     display: flex;
+     align-items: center;
+     margin-left: 25px;
+     padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+
+`;
+
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+
+const Logo = styled.div`
+    font-family: 'Merriweather';
+    font-size: 50px;
+    font-weight: bold;
+    color: orange;
+`;
+
+const Right = styled.div`    
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end
+`;
+
+const MenuItem = styled.div`
+    font-size: 14;
+    cursor: pointer;
+    margin-left: 25px;
+    
+`;
+
+ 
+const Navbar = () => {
+
+
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+    </Routes>
+    <Container>
+        <Wrapper>
+            <Left>
+                <SearchContainer>
+                    <Input/>
+                    <Search style={{color:"gray", fontSize:16}}/>
+                </SearchContainer>
+            </Left>
+            <Center><Logo>home as haven</Logo></Center>
+            <Right>
+
+                <MenuItem>
+                    <Link to="/register">REGISTER</Link>
+                </MenuItem>
+                <MenuItem>
+                    <Link to="/login">LOG IN</Link>
+                </MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={2} color="primary">
+                        <ShoppingBagOutlined />
+                    </Badge>
+                </MenuItem>
+        </Right>
+        </Wrapper>
+    </Container>
+    </BrowserRouter>
+  )
+}
+
+export default Navbar
---
+++ frontEnd/src/data.js
@@ -0,0 +1,26 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/kHR1TJG/welcome.jpg",
+        title: "WELCOME",
+        desc: "FREE SHIPPING THIS WEEKEND",
+        bg: "C4DEF6",
+    },
+
+    {
+        id: 2,
+        img: "https://i.ibb.co/7GqPFzH/pexels-pixabay-159644.jpg",
+        title: "HELLO",
+        desc: "FREE SHIPPING THIS WEEKEND",
+        bg: "dfed8e",
+    },
+
+{
+    id: 3,
+    img: "https://i.ibb.co/Hp0XFvM/beads.jpg",
+    title: "KIA ORA",
+    desc: "FREE SHIPPING THIS WEEKEND",
+    bg: "ed8eb7",
+},
+
+]
