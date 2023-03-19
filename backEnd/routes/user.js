0269964c849df9eab4fc50abf2720d5256975492
abbytagos <abbytagos@gmail.com>
2023-03-20 12:02:33 +1300

    updated product order cart routes

--- backEnd/routes/user.js
+++ backEnd/routes/user.js
@@ -30,6 +30,16 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 //GET USER
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
